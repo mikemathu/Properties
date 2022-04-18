93615f1f646ddddad9c16ddbb24d48d8abe868fe
Michael Mathu <mathumichael17@gmail.com>
2022-04-18 21:19:06 +0300

    properties

---
+++ Book.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNamespace
+{
+    internal class Book
+    {
+        //Instanct Variables
+        private string title;
+        private string author;
+
+        /*
+        //Constructor. Allows the user of this class to instanciate the instant variables and give them a starting state/initial values
+        public Book(string title, string author)
+        {
+            //class level titile = method level title;
+            this.title = title;
+            this.author = author;
+        }
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+        */
+
+       /* //Methods
+        //Retrive the current title of the book
+        public string GetTitle()
+        {
+            return title;
+        }
+        //Specify the new title for the book(Change)
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        //Supply the text of the book and update the word count accordingly
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }*/
+
+        //PROPERTIES
+        
+        //Creating Properties
+        /*public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }*/
+
+        //Auto-implemented Properties
+        
+        public string Title { get; set; }
+        public string Author { get; set; }
+        
+
+        //Read-Only Auto-Implemented Properties
+        /*
+        //Using Default Values
+        public string Title { get; set; } = "Think and Grow Rich";
+        public string Author { get; set; } = "Mike";
+        */
+
+        //Using Construstor
+        /*
+        public string Title { get; }
+        public string Author { get; }
+        //Constructor
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+        */
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using BookNamespace;
+
+namespace Properties
+{
+    public class Properties
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //New instance of the Book class
+            //using the 1st constructor
+            Book book = new Book("Harry Potter", "J.K Rowling");
+
+            //Using the 2nd constructor
+            //Book book = new Book("Harry Potter", "J.K Rowling", 25, 1000 );
+
+            //Changed my mind. Let's use the full name.
+            book.SetTitle("Harry Potter and the Half-Blood Price");
+
+            //Now I forgot.What was the title again?
+            Console.WriteLine(book.GetTitle());
+            */
+
+            /*
+            Book book = new Book();
+            book.Title = "Millionaire Fast Lane"; //Set
+            book.Author = "MJ Demarco"; //set
+            */
+
+            //Object Initializer Sysntax
+            Book book = new Book() { Title = "Fastlane", Author = "MJ"};
+
+            
+
+            //Read-Only Auto-Implementation
+            /*
+            Book book = new Book("M Fastlane", "MJ ");
+            */
+
+
+    
+
+            Console.WriteLine(book.Title+ " Is a book written by " + book.Author);
+        }
+    }
+}
